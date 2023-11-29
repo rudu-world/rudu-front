92aaae9d4f123dace46db033957a6da154cfa9a1
Chanhwi Lee <chanhwi.lee6@gmail.com>
2023-11-29 21:52:04 +0900

    ✨ [CHORE] : GlobalStyle 및 vscode 테마

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 import Test from "@pages/test/Test";
+import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <GlobalStyle />
       <Test />
       <div>
         <a href="https://vitejs.dev" target="_blank">
---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    html {
+        font-size: 62.5%
+    }
+`;
+
+export default GlobalStyle;
